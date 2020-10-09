0035a1a9ae2dae7147cabf2e037c2a6b15c7695a
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-09 13:29:23 +0000

    best guess

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * compares two arrays and says if they are they same or not
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter the values for the first array:");
+    int[] numsOne = new int[5];
+      for(int i = 0; i < numsOne.length; i++){
+       numsOne[i] = input.nextInt();
+      }
+    System.out.println("Please enter the values for the second array:");
+    int[] numsTwo = new int[5];
+      for(int i = 0; i < numsTwo.length; i++){
+       numsTwo[i] = input.nextInt();
+      }
     
+      if(numsOne[1] != numsTwo[1]){
+        System.out.println("These arrays are not the same");
+        for(int i = 0; i < 5; i++){
+          if(numsOne[i] == numsTwo[i]){
+      System.out.println("These arrays are the same");
+      } 
+
   }
+
+  }
+}
 }
