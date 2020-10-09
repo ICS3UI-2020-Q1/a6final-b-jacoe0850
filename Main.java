34aac909785a220099d60ac1db2e0eec0381ede9
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-09 13:56:29 +0000

    Version 1 Finished array test

--- Main.java
+++ Main.java
@@ -10,27 +10,43 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    // make scanner for input
     Scanner input = new Scanner(System.in);
+    // ask for first set of integers
     System.out.println("Please enter the values for the first array:");
+    // make array for first set of integers
     int[] numsOne = new int[5];
+    // for loop so they can type 5 integers and input it into array
       for(int i = 0; i < numsOne.length; i++){
        numsOne[i] = input.nextInt();
       }
+      // Asking for 2nd set of integers
     System.out.println("Please enter the values for the second array:");
+    // array for 2nd set of integers
     int[] numsTwo = new int[5];
+    // for oop so they can type 5 integers and input it into 2nd array
       for(int i = 0; i < numsTwo.length; i++){
        numsTwo[i] = input.nextInt();
       }
-    
-      if(numsOne[1] != numsTwo[1]){
+    // if statements checking if each integer is equal to its correlated one
+      if(numsOne[0] != numsTwo[0]){
         System.out.println("These arrays are not the same");
-        for(int i = 0; i < 5; i++){
-          if(numsOne[i] == numsTwo[i]){
+      }else if(numsOne[1] != numsTwo[1]){
+        System.out.println("These arrays are not the same");
+      } else if(numsOne[2] != numsTwo[2]){
+        System.out.println("These arrays are not the same");
+      } else if(numsOne[3] != numsTwo[3]){
+        System.out.println("These arrays are not the same");
+      } else if(numsOne[4] != numsTwo[4]){
+        System.out.println("These arrays are not the same");
+      }
+      // if statemnt for if theyre equal
+    if (numsOne[4] == numsTwo[4] && numsOne[1] == numsTwo[1]){
       System.out.println("These arrays are the same");
-      } 
-
+    } 
+    
   }
+  
+}
 
-  }
-}
-}
+      
